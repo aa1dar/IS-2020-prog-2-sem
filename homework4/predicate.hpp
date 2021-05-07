abf1f909b44483f804160a469370f5e48f3c415f
Айдар Нафиков <naidar31@gmail.com>
2021-05-07 13:08:39 +0300

    lab 4

---
+++ homework4/predicate.hpp
@@ -0,0 +1,142 @@
+#ifndef C___PREDICATE_HPP
+#define C___PREDICATE_HPP
+
+#include <iostream>
+
+
+template <class TInputIterator, class TPredicate>
+bool allOf(TInputIterator begin, TInputIterator end, TPredicate func){
+    for(;begin!=end;begin++){
+        if(!func(*begin)){
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+template <class TInputIterator, class TPredicate>
+bool anyOf(TInputIterator begin, TInputIterator end, TPredicate func){
+    for(;begin!=end;begin++){
+        if(func(*begin)){
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+
+
+template <class TInputIterator, class TPredicate>
+bool noneOf(TInputIterator begin, TInputIterator end, TPredicate func){
+    for(;begin!=end;begin++){
+        if(func(*begin)){
+            return false;
+        }
+
+    }
+
+    return true;
+}
+
+template <class TInputIterator, class TPredicate>
+bool oneOf(TInputIterator begin, TInputIterator end, TPredicate func){
+    bool flag = false;
+    for(;begin!=end;begin++){
+        if(func(*begin)){
+            if(flag)
+                return false;
+            else
+                flag = true;
+        }
+
+    }
+
+    return true;
+}
+
+template <class TForwardIterator, class TCompare>
+bool isSorted(TForwardIterator begin, TForwardIterator end, TCompare comp){
+
+
+    if(begin!=end){
+        TForwardIterator second = begin;
+        while (++second != end){
+            if(comp(*second,*begin))
+                return false;
+            begin = second;
+        }
+
+
+    }
+    return true;
+
+
+}
+
+template <class TForwardIterator>
+bool isSorted(TForwardIterator begin, TForwardIterator end){
+    return isSorted(begin,end,std::less<>());
+}
+
+
+template <class TInputIterator, class TPredicate>
+bool isPartitioned(TInputIterator begin, TInputIterator end, TPredicate func) {
+    bool flag = false;
+
+    for (; begin != end; begin++) {
+        if (!func(*begin) && !flag)
+            flag = true;
+        if (flag && func(*begin))
+            return false;
+    }
+
+    return true;
+}
+
+template <class TInputIterator, class T>
+TInputIterator findNot(TInputIterator begin, TInputIterator end, const T& value) {
+    for(;begin!=end;begin++){
+        if(*begin != value)
+            return begin;
+    }
+    return end;
+}
+
+
+template <class TForwardIterator, class T>
+TForwardIterator findBackward(TForwardIterator begin, TForwardIterator end, const T& value) {
+    TForwardIterator result = end;
+    for(;begin!=end;begin++){
+        if(*begin == value)
+            result = begin;
+    }
+
+    return result;
+}
+
+template <class TForwardIterator, class TPredicate>
+bool isPalindrome(TForwardIterator begin, TForwardIterator end, TPredicate func) {
+    TForwardIterator first = begin;
+    end--;
+    while(first!=end){
+        if(!func(*begin,*end))
+            return false;
+        begin++;
+        end--;
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+#endif //C___PREDICATE_HPP
